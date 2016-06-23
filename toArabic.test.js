a3ebe8be57ed64332018f0ac011d83c0d9f4af3b
Tim Huddle <blazhen88@gmail.com>
2016-06-23 17:57:23 -0400

    start up npm package, install ava, write ava tests

---
+++ toArabic.test.js
@@ -0,0 +1,34 @@
+const test = require('ava')
+const {toArabic} = require('./converter')
+
+test('I -> 1', t => {
+  t.is(toArabic('I'), 1)
+})
+
+test('III -> 3', t => {
+  t.is(toArabic('III'), 3)
+})
+
+test('IX -> 9', t => {
+  t.is(toArabic('IX'), 9)
+})
+
+test('MLXVI -> 1066', t => {
+  t.is(toArabic('MLXVI'), 1066)
+})
+
+test('MCMLXXXIX -> 1989', t => {
+  t.is(toArabic('MCMLXXXIX'), 1989)
+})
+
+test('HIPALS should throw', t => {
+  t.throws(toArabic('HIPALS'), 'Invalid input')
+})
+
+test('10 should throw', t => {
+  t.throws(toArabic('10'), 'Invalid input')
+})
+
+test('EmptyString -> 0', t => {
+  t.is(toArabic(''), 0)
+})
---
+++ toRoman.test.js
@@ -0,0 +1,34 @@
+const test = require('ava')
+const {toRoman} = require('./converter')
+
+test('1 -> I', t => {
+  t.is(toRoman('1'), 'I')
+})
+
+test('3 -> III', t => {
+  t.is(toRoman('3'), 'III')
+})
+
+test('9 -> IX', t => {
+  t.is(toRoman('9'), 'IX')
+})
+
+test('1066 -> MLXVI', t => {
+  t.is(toRoman('1066'), 'MLXVI')
+})
+
+test('1989 -> MCMLXXXIX', t => {
+  t.is(toRoman('1989'), 'MCMLXXXIX')
+})
+
+test('-1 should throw', t => {
+  t.throws(toRoman('-1'), 'Invalid input')
+})
+
+test('0 -> EmptyString', t => {
+  t.is(toRoman('0'), '')
+})
+
+test('X should throw', t => {
+  t.throws(toRoman('X'), 'Invalid input')
+})
