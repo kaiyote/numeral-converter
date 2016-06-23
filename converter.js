127f0fd50d007a5e291e75ce6b9c4a09f5afa7b9
Tim Huddle <blazhen88@gmail.com>
2016-06-23 19:10:33 -0400

    toRoman all works now

--- converter.js
+++ converter.js
@@ -1,5 +1,6 @@
 function toArabic(roman) {
   if (!isValidRomanNumeral(roman)) throw new Error('Invalid input')
+  return parseRomanNumeral(roman)
 }
 
 function toRoman(arabic) {
@@ -40,6 +41,46 @@ function formatNumberParts(count, onePart, fivePart, tenPart) {
 
 function isValidRomanNumeral(roman) {
   return !roman.match(/[^MDCLXVI]/ig)
+}
+
+function parseRomanNumeral(romanString) {
+  let charList = romanString.split('')
+  if (charList.length === 0) return 0
+  let part = charList.shift()
+
+  if (part == 'I' && ['V', 'X'].includes(charList[0]) ||
+      part == 'X' && ['L', 'C'].includes(charList[0]) ||
+      part == 'C' && ['D', 'M'].includes(charList[0])) part += charList.shift()
+
+  let partial = 0
+  switch (part) {
+    case 'IV':
+      return 4 + parseRomanNumeral(charList.join(''))
+    case 'IX':
+      return 9 + parseRomanNumeral(charList.join(''))
+    case 'XL':
+      return 40 + parseRomanNumeral(charList.join(''))
+    case 'XC':
+      return 90 + parseRomanNumeral(charList.join(''))
+    case 'CD':
+      return 400 + parseRomanNumeral(charList.join(''))
+    case 'CM':
+      return 900 + parseRomanNumeral(charList.join(''))
+    case 'I':
+      return 1 + parseRomanNumeral(charList.join(''))
+    case 'V':
+      return 5 + parseRomanNumeral(charList.join(''))
+    case 'X':
+      return 10 + parseRomanNumeral(charList.join(''))
+    case 'L':
+      return 50 + parseRomanNumeral(charList.join(''))
+    case 'C':
+      return 100 + parseRomanNumeral(charList.join(''))
+    case 'D':
+      return 500 + parseRomanNumeral(charList.join(''))
+    case 'M':
+      return 1000 + parseRomanNumeral(charList.join(''))
+  }
 }
 
 module.exports = {
